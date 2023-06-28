98a4efdeb45d9951837c2037b04e7166308afdbc
xukk3 <xukk3@ziroom.com>
2023-06-28 13:07:28 +0800

    添加注释

--- spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassPostProcessor.java
+++ spring-context/src/main/java/org/springframework/context/annotation/ConfigurationClassPostProcessor.java
@@ -312,6 +312,7 @@ public class ConfigurationClassPostProcessor implements BeanDefinitionRegistryPo
 		Set<BeanDefinitionHolder> candidates = new LinkedHashSet<>(configCandidates);
 		Set<ConfigurationClass> alreadyParsed = new HashSet<>(configCandidates.size());
 		do {
+			// parser.parse方法对ComponentScans、ImportSelector、importResource注解进行解析
 			parser.parse(candidates);
 			parser.validate();
 
